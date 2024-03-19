c886bb6945e1359878e5a4aaa06db3b86f536d94
unknown <Administrador@MGIRNSRA104802D.mte.br>
2024-03-19 08:34:15 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ closeModalBtn.addEventListener("click", function(){
 menu.addEventListener("click", function(event){
     let parentButton = event.target.closest(".add-to-cart-btn")
 
+
     if(parentButton){
         const name = parentButton.getAttribute("data-name")
         const price = parseFloat(parentButton.getAttribute("data-price"))
@@ -54,21 +55,143 @@ function addToCart(name, price){
         cart.push({
             name,
             price,
-            quantity:1
+            quantity:1,
         })
     }
 
-updateCartModal()    
+    updateCartModal()    
 }
 
 //atualiza carrinho
 
 
 function updateCartModal(){
-    cartItemsContainer.innerHTML = ""
+    cartItemsContainer.innerHTML = "";
     let total = 0;
 
-    cart.forEach(item =>{
-        const cartItemeLEMENT = document.createElement("div")
-    })
+    cart.forEach(item => {
+         const cartItemElement= document.createElement("div");
+         cartItemElement.innerHTML = `
+        
+         <div class="flex items-center justify-between">
+             <div>
+                 <p class="font-medium">${item.name}</p>
+                 <p>Qtd:${item.quantity}</p>
+                 <p class="font-medium mt-2">R$ ${item.price.toFixed(2)}</p>
+             </div>
+
+           
+                 <button class="remove-from-cart-btn" data-name="${item.name}">
+                     Remover
+                 </button>
+
+        </div>
+             
+         `
+
+         total += item.price * item.quantity
+
+         cartItemsContainer.appendChild(cartItemElement)
+     })
+
+     cartTotal.textContent = total.toLocaleString("pt-BR", {
+        style:"currency",
+        currency:"BRL"
+
+     });
+
+     cartCounter.innerText = cart.length;
+}
+
+// função para remover item do carrinho
+
+cartItemsContainer.addEventListener("click", function(event){
+    if(event.target.classList.contains("remove-from-cart-btn")){
+        const name = event.target.getAttribute("data-name")
+
+        removeItemCart(name)
+    }
+})
+
+function removeItemCart(name){
+    const index = cart.findIndex(item => item.name === name)
+
+    if(index !== -1){
+        const item = cart[index];  
+
+        if(item.quantity > 1){
+            item.quantity -= 1
+            updateCartModal()
+            return;
+        }
+
+        cart.splice(index, 1)
+        updateCartModal()
+    }
+}
+
+addressInput.addEventListener("input", function(event){
+    let inputValue = event.target.value;
+
+    if(inputValue !== ""){
+        addressInput.classList.remove("border-red-500")
+        addressWarn.classList.add("hidden")
+
+    }
+})
+
+
+// finalizar carrinho
+
+checkoutBtn.addEventListener("click", function(){
+
+    const  isOpen = checkRestaurantOpen()
+    if(!isOpen){
+        alert("RESTAURANTE FECHADO NO  MOMENTO")
+        return;
+    }
+
+    
+
+    if(cart.length === 0 ) return
+    if(addressInput.value === ""){
+    addressWarn.classList.remove("hidden")
+    addressInput.classList.add("border-red-500");
+    return;
+}
+
+    // envia  o pedido para API whats
+    const cartItems = cart.map((item)=>{
+        return(
+            `
+            ${item.name}
+            Quantidade:(${item.quantity})
+            Preço: R$${item.price}
+            `
+        )
+    }).join("")
+
+    const message = encodeURIComponent(cartItems)
+    const phone = "5584987064895";
+
+    window.open(`https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}`, "_blank")
+
+})
+
+// verifica a hora e manipula card horario
+function checkRestaurantOpen(){
+    const data = new Date();
+    const hora = data.getHours();
+    return hora >= 08 && hora < 22   
+}
+
+const spanItem = document.getElementById("date-span");
+const isOpen = checkRestaurantOpen();
+
+if(isOpen){
+    spanItem.classList.remove("bg-red-500")
+    spanItem.classList.add("bg-green-600")
+}else{
+    spanItem.classList.remove("bg-green-600")
+    spanItem.classList.add("bg-red-500")
 }
